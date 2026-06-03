fc4aaf15ac09bb2d9d187dbb930af23930e7fee5
faure872000 <faure872000@gmail.com>
2026-06-03 10:34:38 +0800

    Update q5.py

--- q5.py
+++ q5.py
@@ -5,10 +5,22 @@ def check_divisibility(num, divisor):
     - Both num and divisor must be numeric.
     - Return True if num is divisible by divisor, False otherwise.
     """
-    return
+    #check if divisor is 0 to avoid errors
+    if divisor == 0:
+        return False
+    #check if both num and divisor are numeric types
+    elif isinstance(num, (int,float)) and isinstance(divisor, (int,float)):
+        if num % divisor == 0:   
+            return True
+        else:
+            return False
+    else:
+        return "Not numeric"
 
 
 # Task 2
 # Invoke the function "check_divisibility" using the following scenarios:
 # - 10, 2
 # - 7, 3
+print(check_divisibility(10, 2))
+print(check_divisibility(7, 3))
