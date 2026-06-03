f3e2d8b5401e4dd2047b12f9d795a1ca694ab253
faure872000 <faure872000@gmail.com>
2026-06-03 11:01:20 +0800

    Update q7.py

--- q7.py
+++ q7.py
@@ -5,11 +5,18 @@ class Car:
     - Initialize these attributes in the __init__ method
     - Add a method named describe_car() that prints information about the car as "Year Make Model"
     """
+    #init method
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
 
     def describe_car(self):
-        print("")
+        print("Year: " + str(self.year) + ", Make: " + self.make + ", Model: " + self.model)
 
 
 # Task 2
 # Create an instance of the Car class with the following attributes and call describe_car method:
 # - Make: Toyota, Model: Corolla, Year: 2020
+mycar = Car("Toyota", "Corolla", 2020)
+mycar.describe_car()
